6cbc14da6474357aacd3a7599d613e5072cf725f
supercuem <97792229+supercuem@users.noreply.github.com>
2022-08-09 13:36:45 +0700

    aa

--- src/main/java/com/poly/service/ProductService.java
+++ src/main/java/com/poly/service/ProductService.java
@@ -9,9 +9,18 @@ public interface ProductService {
 
     List<Product> findAll();
 
+//    Product findById(Integer id);
+
+
     Product findById(Integer id);
+    
+    List<Product> findByCategoryId(String cid);
 
+    Product create(Product product);
 
+    Product update(Product product);
+
+    void delete(Integer id);
 
     List<Product> findByCategoryId(Integer cid);
 }
